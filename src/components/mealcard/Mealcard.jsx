9fcbf71e07a2d7df5b6a31a242578f313c909519
Coucodeur <matcou68@gmail.com>
2024-07-10 20:52:30 +0200

    adding icons for tapbar / starting to build btn and logic to make <la liste des courses> sur les mealCards

--- src/components/mealcard/Mealcard.jsx
+++ src/components/mealcard/Mealcard.jsx
@@ -1,65 +1,68 @@
-import { Link } from 'react-router-dom';
-import './Mealcard.scss';
-import deleteIcon from '../../assets/delete-1487-svgrepo-com.svg';
+import { Link } from "react-router-dom";
+import "./mealcard.scss";
+import deleteIcon from "../../assets/delete-1487-svgrepo-com.svg";
+import addToShop from "../../assets/add_to_shopping_list.svg";
 
-const Mealcard = ({
-  id,
-  name,
-  prepTime,
-  ingredients,
-  lastPrep,
-  action,
-  deleteMeal,
-}) => {
-  const formatedName = name.charAt(0).toUpperCase() + name.slice(1);
-  const formatedDate = new Date(lastPrep).toLocaleDateString('fr-FR', {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-  });
+const Mealcard = ({ id, name, prepTime, ingredients, lastPrep, action, deleteMeal }) => {
+    const formatedName = name.charAt(0).toUpperCase() + name.slice(1);
+    const formatedDate = new Date(lastPrep).toLocaleDateString("fr-FR", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+    });
 
-  return (
-    <div className="meal-card">
-      <h2>{formatedName}</h2>
-      <div className="meal-icon-container">
-        {/* <Link to={`/meal/${id}`}>
+    const addIngredientsToShoppingList = (ingredients) => {
+        console.log(ingredients);
+    };
+
+    return (
+        <div className="meal-card">
+            <h2>{formatedName}</h2>
+            <div className="meal-icon-container">
+                {/* <Link to={`/meal/${id}`}>
           <img src={editIcon} className="cta-icon" alt="edit-icon" />
         </Link> */}
-        <img
-          src={deleteIcon}
-          alt="delete-icon"
-          className="cta-icon cta-delete"
-          onClick={() => deleteMeal(id)}
-        />
-      </div>
-      <p>Temps de préparation : {prepTime} minutes</p>
-      <div className="ingredient-container-mealcard">
-        <h3>Ingrédients :</h3>
-        <ul className="meallist-ingredient-container">
-          {ingredients.map(
-            (ingredient, index) =>
-              index < 5 && (
-                <li key={index}>
-                  <span>{index + 1} - </span>
-                  {ingredient}
-                </li>
-              )
-          )}
-        </ul>
-      </div>
-      <div className="last-prep-container">
-        <p>Dernière préparation le :</p>
-        <p>{formatedDate}</p>
-      </div>
-      <Link className="cta-details" to={`/meal/${id}`}>
-        Modifier - Consulter
-      </Link>
-      <button className="cta-miam" onClick={() => action(id)}>
-        MIAM
-      </button>
-    </div>
-  );
+                <img
+                    src={deleteIcon}
+                    alt="delete-icon"
+                    className="cta-icon cta-delete"
+                    onClick={() => deleteMeal(id)}
+                />
+            </div>
+            <p>Temps de préparation : {prepTime} minutes</p>
+            <div className="ingredient-container-mealcard">
+                <h3>Ingrédients :</h3>
+                <ul className="meallist-ingredient-container">
+                    {ingredients.map(
+                        (ingredient, index) =>
+                            index < 5 && (
+                                <li key={index}>
+                                    <span>{index + 1} - </span>
+                                    {ingredient}
+                                </li>
+                            )
+                    )}
+                </ul>
+            </div>
+            <div className="last-prep-container">
+                <p>Dernière préparation le :</p>
+                <p>{formatedDate}</p>
+            </div>
+            <Link className="cta-details" to={`/meal/${id}`}>
+                Modifier - Consulter
+            </Link>
+            <button
+                className="cta-details"
+                onClick={() => addIngredientsToShoppingList(ingredients)}
+            >
+                Ajouter les ingredients à la liste de courses
+            </button>
+            <button className="cta-miam" onClick={() => action(id)}>
+                MIAM
+            </button>
+        </div>
+    );
 };
 
 export default Mealcard;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,26 @@
-import './nav.scss';
-import { NavLink } from 'react-router-dom';
-
+import "./nav.scss";
+import { NavLink } from "react-router-dom";
+import addMealIcon from "../../assets/add_new_meal.svg";
+import menuPageIcon from "../../assets/meal_page.svg";
+import ingrendientPageIcon from "../../assets/ingredient_list_icon.svg";
 const Nav = () => {
-  return (
-    <nav className="navbar">
-      <NavLink to={'/'} className="navLink">
-        Liste repas
-      </NavLink>
-      <NavLink to={'/addmeal'} className="navLink">
-        Ajout repas
-      </NavLink>
-      <NavLink to={'/ingredients'} className="navLink">
-        Ingredients
-      </NavLink>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <NavLink to={"/"} className="navLink">
+                <img src={menuPageIcon} className="nav-icon" alt="menu-page-icon" />
+            </NavLink>
+            <NavLink to={"/addmeal"} className="navLink">
+                <img src={addMealIcon} className="nav-icon" alt="addmeal-page-icon" />
+            </NavLink>
+            <NavLink to={"/ingredients"} className="navLink">
+                <img
+                    src={ingrendientPageIcon}
+                    className="nav-icon"
+                    alt="ingredeint-page-icon"
+                />
+            </NavLink>
+        </nav>
+    );
 };
 
 export default Nav;
